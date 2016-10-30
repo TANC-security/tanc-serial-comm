91a67fc2908cb3b36e0358113d0e82c564cfd965
Mark Kimsal <metrofindings@gmail.com>
2016-10-30 12:41:59 -0400

    Add systemd service

--- package-deb.sh
+++ package-deb.sh
@@ -2,9 +2,11 @@
 PACKAGE_ROOT=build/deb/tanc-serial-comm
 mkdir -p $PACKAGE_ROOT/DEBIAN/
 mkdir -p $PACKAGE_ROOT/opt/tanc/serial-comm/vendor/
+mkdir -p $PACKAGE_ROOT/opt/tanc/systemd/
 mkdir -p $PACKAGE_ROOT/opt/tanc/bin
 cp bin/serial-comm.php $PACKAGE_ROOT/opt/tanc/bin/
 cp -r serial-comm/vendor/* $PACKAGE_ROOT/opt/tanc/serial-comm/vendor/
+cp -r systemd/* $PACKAGE_ROOT/opt/tanc/systemd/
 cp -r debian/* $PACKAGE_ROOT/DEBIAN/
 cd $PACKAGE_ROOT
 find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
